ffdc6d1debe0ca82450d489dcb696b0a2760c87c
Jeffrey Blagdon <jeffblagdon@gmail.com>
2020-01-19 16:42:03 +0900

    Get conversation working

--- Chapter 1/HelloAVF_Starter/Hello AV Foundation/THSpeechController.m
+++ Chapter 1/HelloAVF_Starter/Hello AV Foundation/THSpeechController.m
@@ -27,7 +27,9 @@
 #import <AVFoundation/AVFoundation.h>
 
 @interface THSpeechController ()
-
+@property (strong, nonatomic) AVSpeechSynthesizer *synthesizer;
+@property (strong, nonatomic) NSArray *voices;
+@property (strong, nonatomic) NSArray *speechStrings;
 @end
 
 @implementation THSpeechController
@@ -36,8 +38,39 @@
     return [[self alloc] init];
 }
 
-- (void)beginConversation {
+- (id) init {
+    self = [super init];
+    if (self) {
+        _synthesizer = [[AVSpeechSynthesizer alloc] init];
+        _voices = @[[AVSpeechSynthesisVoice voiceWithLanguage:@"en-US"],
+                    [AVSpeechSynthesisVoice voiceWithLanguage:@"en-GB"]];
+        _speechStrings = [self buildSpeechStrings];
+    }
+    return self;
+}
 
+- (NSArray *)buildSpeechStrings {
+    return @[
+        @"Hello AV Foundation. How are you?",
+        @"I'm well!  Thanks for asking.",
+        @"Are you excited about the book?",
+        @"Very! I have always felt so misunderstood",
+        @"What's your favorite feature?",
+        @"Oh, they're all my babies. I couldn't possibly choose.",
+        @"It was great to speak with you!",
+        @"The pleasure was all mine!  Have fun!"
+    ];
+}
+
+- (void)beginConversation {
+    for (NSUInteger i = 0; i < self.speechStrings.count; i++) {
+        AVSpeechUtterance *utterance = [[AVSpeechUtterance alloc] initWithString:self.speechStrings[i]];
+        utterance.voice = self.voices[i % 2];
+        utterance.rate = 0.4f;
+        utterance.pitchMultiplier = 0.8f;
+        utterance.postUtteranceDelay = 0.1f;
+        [self.synthesizer speakUtterance:utterance];
+    }
 }
 
 @end
